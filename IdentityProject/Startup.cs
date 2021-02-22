2ea63b442127cbd2a94842c957b1992c332aa6e6
SedaBacı <sedabacii@gmail.com>
2021-02-22 13:04:11 +0300

    dotnoet ef migrations add Initial

--- IdentityProject/Startup.cs
+++ IdentityProject/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IdentityProject.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,21 +17,29 @@ namespace IdentityProject
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMvcCore();           //MVC uygulaması ile ilgili Core bileşenlerini kurar uygulama ayağa kalkarken patlar
-            services.AddMvc();                //MVC uygulaması ile ilgili tüm bileşenleri kurar            
-        }
+            services.AddDbContext<AppIdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(configuration["ConnectionStrings:DefaultConnectionString"]);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();  //AppIdentityDbContext'e kaydet
+            services.AddMvc();                       //MVC uygulaması ile ilgili tüm bileşenleri kurar
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
-            app.UseDeveloperExceptionPage();      // sayfada hata alındıgında acıklayıcı bilgi sunuyor
-            app.UseStatusCodePages();             // content dönmeyen sayfalarda acıklayıcı bilgi sunuyor
-            app.UseStaticFiles();                 // js css gibi dosyaları yüklemek için
+            app.UseDeveloperExceptionPage();         // sayfada hata alındıgında acıklayıcı bilgi sunuyor
+            app.UseStatusCodePages();                // content dönmeyen sayfalarda acıklayıcı bilgi sunuyor
+            app.UseStaticFiles();                    // js css gibi dosyaları yüklemek için
+            app.UseAuthentication();                 // Identity mekanizması kullanacağım için ekliyorum.
             app.UseEndpoints(endpoints =>
             {
                 app.UseEndpoints(endpoints =>
@@ -37,8 +49,7 @@ namespace IdentityProject
                         pattern: "{controller=Home}/{action=Index}/{id?}");
                 });
             });
-
-            //app.UseMvcWithDefaultRoute();         // default  --Controller/Action/{id}-- isimli route olusturur, manuel eklemeye gerek kalmaz
+            //app.UseMvcWithDefaultRoute();          // default  --Controller/Action/{id}-- isimli route olusturur, manuel eklemeye gerek kalmaz
         }
     }
 }
